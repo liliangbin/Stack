6041b4d57c957831242dd9a5ed58e7877da393c9
liliangbin <2286824491@qq.com>
2019-03-21 09:41:15 +0800

    min stack

---
+++ leecode/src/minstack/Main.java
@@ -0,0 +1,33 @@
+package minstack;
+import java.util.Stack;
+
+/**
+ * @author liliangbin  dumpling1520@gmail.com
+ * @date 2019/3/21  9:34
+ **/
+public class Main {
+    // INFO  2019/3/21 9:34 liliangbin
+    /*
+    * 定义栈的数据结构，
+    * 请在该类型中实现一个能够得到栈
+    * 中所含最小元素的min函数
+    * （时间复杂度应为O（1））。*/
+
+    public void push(int node) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int min() {
+
+    }
+
+
+}
